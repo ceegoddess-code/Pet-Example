07d810dd555df8047fbe8769f2324874645b057e
ceegoddess <slsmith0602@gmail.com>
2025-10-09 14:03:20 -0500

    adding updated index.js to pet example

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-//create a pet object
+pet = {
+    name: "Chocolate", // Pet's name
+    type: "Cat",    // Pet's type
+    happiness: 100, // Pet's happiness level
+    
+    play: function() {
+        this.happiness += 10; // Increase happiness by 10
+        return 'You played with ${this.name} ! Happiness is now ${this.happiness}.';
+
+},
+
+    feed: function() {
+        this.happiness += 20; // Increase happiness by 20
+        return 'You fed ${this.name} ! Happiness is now ${this.happiness}.';
+
+    },
+    // Method to display the pet's current status
+    status: function() {
+        console.log ('Pet Nmae: ${this.name}, species: ${this.type}, Happiness: ${this.happiness}');
+    },
+    // Method to rename the pet
+    rename: function(newName) {
+        this.name = newName;
+        return 'Your pet has been renamed to ${this.name}.';
+        console.log(pet.rename("Chocolate"));
+        pet.status();
+
+    }
+};
+
+
+
+//Day 2 create a pet object
 function createPet(name, species) {
 let happiness = 50;
 let energy = 100;
